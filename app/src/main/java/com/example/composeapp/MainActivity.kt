7232c909bd7119b11d2285516f109152f39c255e
Lucas Nobrega <105940701+lucas-pdnobrega@users.noreply.github.com>
2024-07-30 23:35:24 -0300

    add stage 3 - quadrants

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign.Companion.Justify
@@ -32,8 +36,11 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     /*Article(
                         modifier = Modifier.padding(innerPadding)
-                    )*/
+                    )
                     TaskManager(
+                        modifier = Modifier.padding(innerPadding)
+                    )*/
+                    Quadrants(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -91,6 +98,56 @@ fun TaskManager(modifier: Modifier = Modifier) {
         Text(
             text = "Nice work!",
             fontSize = 16.sp)
+    }
+}
+
+@Composable
+fun Quadrants(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row {
+            Column(modifier = Modifier.background(Color(0xFFEADDFF))) {
+                Text(
+                    text = "Text composable\n",
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(0.dp, 16.dp)
+                )
+                Text(
+                    text = "Displays text and follows the recommended Material Design guidelines.\n"
+                )
+            }
+            Column(modifier = Modifier.background(Color(0xFFD0BCFF))) {
+                Text(
+                    text = "Image composable\n",
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(0.dp, 16.dp)
+                )
+                Text(
+                    text = "Creates a composable that lays out and draws a given Painter class object."
+                )
+            }
+        }
+        Row {
+            Column(modifier = Modifier.background(Color(0xFFB69DF8))) {
+                Text(
+                    text = "Row composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(0.dp, 16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a horizontal sequence."
+                )
+            }
+            Column(modifier = Modifier.background(Color(0xFFF6EDFF))) {
+                Text(
+                    text = "Column composable",
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(0.dp, 16.dp)
+                )
+                Text(
+                    text = "A layout composable that places its children in a vertical sequence."
+                )
+            }
+        }
     }
 }
 
